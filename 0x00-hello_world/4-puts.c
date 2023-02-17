8af91b3195400b664e756242a0b323caffedb1d3
Mark Khalil <markekhalil96@gmail.com>
2023-02-16 18:43:24 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *  This is my first C program. I am using put function to print a string.
+ *  
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
