b1bb44cabc299c2e1e7e6c00127249d713048289
Mark Khalil <markekhalil96@gmail.com>
2023-02-18 21:21:15 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lower case alphabet except q & e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 24; i > 24; i--)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
