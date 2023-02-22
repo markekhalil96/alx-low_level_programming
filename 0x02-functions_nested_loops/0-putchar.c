fc03b43d2addb58b62a264cbd286b21531d8a4fc
Mark Khalil <markekhalil96@gmail.com>
2023-02-21 18:22:03 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Call the write system call to print the string "_putchar\n" to stdout */
+	write(1, "_putchar\n", 9);
+    
+	return (0);
+}
